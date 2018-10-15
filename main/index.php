db5ee61b0724792930f114f60dcfa3d9d145c689
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-15 15:43:39 +0300

    Update index.php

--- main/index.php
+++ main/index.php
@@ -2,7 +2,6 @@
 <html><head>
     <title></title>
   <link rel="stylesheet" href="ru/styles.css" type="text/css">
-  <link rel="stylesheet" href="ru/menu.css" type="text/css">
   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
   <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
   <script src="ru/functions.js"></script>
