de6b2881c493082401ebc344905c41d59aa1a54a
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-15 15:59:56 +0300

    Update index.php

--- main/index.php
+++ main/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html><head>
     <title></title>
-  <link rel="stylesheet" href="ru/styles.css" type="text/css">
+  <link rel="stylesheet" href="main/styles.css" type="text/css">
   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-  <script src="ru/functions.js"></script>
-  <script src="ru/pre-load.js"></script>
-  <script src="ru/post-load.js"></script>
+  <script src="main/functions.js"></script>
+  <script src="main/pre-load.js"></script>
+  <script src="main/post-load.js"></script>
 </head>
 <body data-role="page">
     <div class="item">
