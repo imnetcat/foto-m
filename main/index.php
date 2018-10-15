0e32f0701ecd737d0064f12759a4a4599f84af2a
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-15 15:31:12 +0300

    Update index.php

--- main/index.php
+++ main/index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html><head>
-    <title>IN ONE Company</title>
+    <title></title>
   <link rel="stylesheet" href="ru/styles.css" type="text/css">
   <link rel="stylesheet" href="ru/menu.css" type="text/css">
-  
   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
   <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
   <script src="ru/functions.js"></script>
@@ -11,87 +10,6 @@
   <script src="ru/post-load.js"></script>
 </head>
 <body data-role="page">
-  <div id="menu">
-    <img id="logo" src="logo.jpg">
-    <span id="about"> 
-      <span>Мы, как и многие другие компании,  занимаемся
-      проектированием домов, дизайном квартир, 
-      строительством зданий и ремонтом квартир....
-      Чем же мы отличаемся от конкурентов, спросите Вы? 
-      </span>
-      <br>
-      <br>
-      <span>
-      Мы, как и одни наши коллеги проектируем всё
-      от любой мелкой детали интерьера до жилых и 
-      общественных комплексов. 
-      </span>
-<br>
-<br>
-<span>
-Мы, как и другие наши коллеги производим и 
-организовываем полный комплекс работ 
-связанных с ремонтом Вашей квартиры, 
-офиса, магазина, строительства дома Вашей мечты 
-или пространства для работы или отдыха.
-   </span>
-   <br>
-    <br>
-    <span>
-    Мы, как и третьи наши коллеги производим сами и 
-    организуем производство индивидуальных 
-    деталей интерьера или элементов благоустройства.
-    </span>
-    <br>
-    <br>
-<span>
-Одно из ВАЖНЫХ наших качеств, чем мы отличаемся от всех, 
-мы обьединяем это всё в одной команде!
-Мы ВСЕГДА доводим наши объекты до конца! 
-Мы НИКОГДА не оставляем наших клиентов и 
-после гарантийных обязательств всегда помогаем
-решать любые проблемы и вопросы! 
-Таким образом сотрудничая с нами Вы получаете 
-полный комплекс услуг связанных с ремонтом или строительством
-от проекта до...тапочек и коврика перед входом :)
-</span>
-    <br>
-    <br>
-    <br>
-    <span>Наши контакты</span>
-    <br>
-    <span>телефон, Viber, Telegram, Line, WhatsApp <span> +38-067-11-99-130</span></span>
-    <br>
-    <span>Телефон <span> +38-099-44-64-88-0</span></span>
-    <br>
-    <span>Email: <span> inone.office@gmail.com</span></span>
-    <br>
-    <br>
-    <span>Сайт сделал Гуськов Данил Игоревич: <span>guskov.danil@gmail.com</span></span>
-  </span>
-  </div>
-  <div class="" id="row">
-  </div>
-  <div id="fullImg">
-    <div>
-      <div class="btn left"><span></span></div>
-      <img src=""/>
-      <div class="btn right"><span></span></div>
-    </div>
-    <div id="floatingCirclesG">
-      <div class="f_circleG" id="frotateG_01"></div>
-      <div class="f_circleG" id="frotateG_02"></div>
-      <div class="f_circleG" id="frotateG_03"></div>
-      <div class="f_circleG" id="frotateG_04"></div>
-      <div class="f_circleG" id="frotateG_05"></div>
-      <div class="f_circleG" id="frotateG_06"></div>
-      <div class="f_circleG" id="frotateG_07"></div>
-      <div class="f_circleG" id="frotateG_08"></div>
-    </div>
-    <div class="fil-close-btn">
-      <div></div>
-      <div></div>
-    </div>
-  </div>
+    
 </body>
 </html>
