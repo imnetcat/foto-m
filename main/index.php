700fd6e76a6053acbecd788d1e2334196cd3474f
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-15 15:56:21 +0300

    Update index.php

--- main/index.php
+++ main/index.php
@@ -3,26 +3,25 @@
     <title></title>
   <link rel="stylesheet" href="ru/styles.css" type="text/css">
   <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-  <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
   <script src="ru/functions.js"></script>
   <script src="ru/pre-load.js"></script>
   <script src="ru/post-load.js"></script>
 </head>
 <body data-role="page">
     <div class="item">
-        <img class="img" src="item-1.jpg">
+        <img class="img" src="main/item-1.jpg">
         <div class="description"></div>
     </div>
     <div class="item">
-        <img class="img" src="item-2.jpg">
+        <img class="img" src="main/item-2.jpg">
         <div class="description"></div>
     </div>
     <div class="item">
-        <img class="img" src="item-2.jpg">
+        <img class="img" src="main/item-2.jpg">
         <div class="description"></div>
     </div>
     <div class="item">
-        <img class="img" src="item-1.jpg">
+        <img class="img" src="main/item-1.jpg">
         <div class="description"></div>
     </div>
 </body>
