9f611968d7eaa2c52d926249c1249494ab7bef08
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-15 17:30:53 +0300

    db

--- photosession/actions.php
+++ photosession/actions.php
@@ -4,10 +4,8 @@ require_once "db.php";
 
 
 switch ($_POST['action']){
-      case 'get_items':
-      $filters = $_POST['filters'];
-      $sortings = $_POST['sortings'];
-      echo get_items($database, id_parse(found_items($database, $filters, $sortings)), $sortings); 
+      case 'load_all':
+      echo load_all($database);
       break;
 };
 ?>
--- photosession/db.php
+++ photosession/db.php
@@ -1,18 +1,24 @@
 <?
-if ($_SERVER['SERVER_NAME'] != "localhost") {
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$host = $url["host"];
-	$username = $url["user"];
-	$password = $url["pass"];
-	$db = substr($url["path"], 1);
-	$database = mysqli_connect($host, $username, $password, $db);
-}else{
+//if ($_SERVER['SERVER_NAME'] != "localhost") {
+	//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	//$host = $url["host"];
+	//$username = $url["user"];
+	//$password = $url["pass"];
+	//$db = substr($url["path"], 1);
+	//$database = mysqli_connect($host, $username, $password, $db);
+//}else{
 	$host = 'localhost';
-	$username = 'root';
-	$password = '';
-	$db = 'users';
+	$username = 'in1ofice_items';
+	$password = 'rootB1';
+	$db = 'in1ofice_items';
 	$database = mysqli_connect($host, $username, $password, $db);
-}
+	if (mysqli_connect_error()) {
+         die('Connect Error (' . mysqli_connect_errno() . ') '
+            . mysqli_connect_error());
+    }else{
+        echo "done";
+    }
+//}
 mysqli_query($database, "SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
 mysqli_query($database, "SET CHARACTER SET 'utf8'");
 ?>
