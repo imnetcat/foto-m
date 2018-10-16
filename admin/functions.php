44b293c6717c5e11d733d146b30004496d101cc0
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-16 23:10:14 +0300

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -41,7 +41,7 @@ function change($database, $id, $derectory, $image, $description, $sub_img_1, $s
 
 function get($database, $derectory){
   $items = " ";
-  $query ="SELECT id, image, description, sub_img_1, sub_img_2, sub_img_3, sub_img_4, sub_img_5, sub_img_6, sub_img_7, sub_img_8, sub_img_9, sub_img_10, sub_img_11, sub_img_12, sub_img_13, sub_img_14, sub_img_15, sub_img_16, sub_img_17, sub_img_18, sub_img_19, sub_img_20, sub_img_21, sub_img_22, sub_img_23, sub_img_24, sub_img_25 FROM ".$derectory;
+  $query ="SELECT id, image, description FROM ".$derectory;
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       $items .= var_dump($row);
