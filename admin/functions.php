cf592c878c6c2c63d8e45f7ea0cbd4c8b187aacc
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-16 23:08:52 +0300

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -1,7 +1,7 @@
 <?
 
 function add_in($database, $image, $derectory, $description, $sub_img_1, $sub_img_2, $sub_img_3, $sub_img_4, $sub_img_5, $sub_img_6, $sub_img_7, $sub_img_8, $sub_img_9, $sub_img_10, $sub_img_11, $sub_img_12, $sub_img_13, $sub_img_14, $sub_img_15, $sub_img_16, $sub_img_17, $sub_img_18, $sub_img_19, $sub_img_20, $sub_img_21, $sub_img_22, $sub_img_23, $sub_img_24, $sub_img_25){
-  $query = "SELECT MAX(id) FROM photosession";
+  $query = "SELECT MAX(id) FROM ".$derectory;
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       $new_id = array_pop($row) + 1;
@@ -27,10 +27,10 @@ function delete($database, $id, $derectory){
   }
 }
 
-function change($database, $id, $derectory, $image, $type, $stone, $technology, $cost, $filter_5, $description){
-  $query = "UPDATE $derectory SET image=?, type=?, stone=?, technology=?, cost=?, filter_5=?, description=? WHERE id=$id";
+function change($database, $id, $derectory, $image, $sub_img_1, $sub_img_2, $sub_img_3, $sub_img_4, $sub_img_5, $sub_img_6, $sub_img_7, $sub_img_8, $sub_img_9, $sub_img_10, $sub_img_11, $sub_img_12, $sub_img_13, $sub_img_14, $sub_img_15, $sub_img_16, $sub_img_17, $sub_img_18, $sub_img_19, $sub_img_20, $sub_img_21, $sub_img_22, $sub_img_23, $sub_img_24, $sub_img_25){
+  $query = "UPDATE $derectory SET image=?, description=?, sub_img_1=?, sub_img_2=?, sub_img_3=?, sub_img_4=?, sub_img_5=?, sub_img_6=?, sub_img_7=?, sub_img_8=?, sub_img_9=?, sub_img_10=?, sub_img_11=?, sub_img_12=?, sub_img_13=?, sub_img_14=?, sub_img_15=?, sub_img_16=?, sub_img_17=?, sub_img_18=?, sub_img_19=?, sub_img_20=?, sub_img_21=?, sub_img_22=?, sub_img_23=?, sub_img_24=?, sub_img_25=? WHERE id=$id";
   $stmt = mysqli_prepare($database, $query);
-  mysqli_stmt_bind_param($stmt, "sssssss", $image, $type, $stone, $technology, $cost, $filter_5, $description);
+  mysqli_stmt_bind_param($stmt, "ssssssssssssssssssssssss", $image, $description, $sub_img_1, $sub_img_2, $sub_img_3, $sub_img_4, $sub_img_5, $sub_img_6, $sub_img_7, $sub_img_8, $sub_img_9, $sub_img_10, $sub_img_11, $sub_img_12, $sub_img_13, $sub_img_14, $sub_img_15, $sub_img_16, $sub_img_17, $sub_img_18, $sub_img_19, $sub_img_20, $sub_img_21, $sub_img_22, $sub_img_23, $sub_img_24, $sub_img_25);
   if(mysqli_stmt_execute($stmt)){
     mysqli_stmt_close($stmt);
     return "Изображение успешно изменено";
