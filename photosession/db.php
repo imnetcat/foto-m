52d658762aa313adf4f0307a9ee9e3653e9fd13b
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-16 23:59:43 +0300

    Update db.php

--- photosession/db.php
+++ photosession/db.php
@@ -1,24 +1,18 @@
 <?
-//if ($_SERVER['SERVER_NAME'] != "localhost") {
-	//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	//$host = $url["host"];
-	//$username = $url["user"];
-	//$password = $url["pass"];
-	//$db = substr($url["path"], 1);
-	//$database = mysqli_connect($host, $username, $password, $db);
-//}else{
-	$host = 'localhost';
-	$username = 'in1ofice_items';
-	$password = 'rootB1';
-	$db = 'in1ofice_items';
+if ($_SERVER['SERVER_NAME'] != "localhost") {
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$host = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
 	$database = mysqli_connect($host, $username, $password, $db);
-	if (mysqli_connect_error()) {
-         die('Connect Error (' . mysqli_connect_errno() . ') '
-            . mysqli_connect_error());
-    }else{
-        echo "done";
-    }
-//}
+}else{
+	$host = 'localhost';
+	$username = 'root';
+	$password = '';
+	$db = 'users';
+	$database = mysqli_connect($host, $username, $password, $db);
+}
 mysqli_query($database, "SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
 mysqli_query($database, "SET CHARACTER SET 'utf8'");
 ?>
