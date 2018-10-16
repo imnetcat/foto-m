2cb6f836fe81c1364233c3442e89522db8165071
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-16 23:50:16 +0300

    Update functions.js

--- photosession/functions.js
+++ photosession/functions.js
@@ -33,6 +33,7 @@ function load_all(){
       for( n = 1; n < raw_data.length; n++){
         allItems[n-1] = new Item(php_array_to_js_array(raw_data[n]));
       }
+      console.log("nice");
       setFirst(allItems);
     }
   });
