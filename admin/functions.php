f959263c5326429470d5c1f312630bd1094c564c
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-16 22:30:59 +0300

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -1,31 +1,13 @@
 <?
 
-function add_in_shop($database, $image, $type, $stone, $technology, $cost, $filter_5, $description){
-  $query = "SELECT MAX(id) FROM shop";
+function add_in_photosession($database, $image, $description){
+  $query = "SELECT MAX(id) FROM photosession";
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       $new_id = array_pop($row) + 1;
     }
   }
-  $query = "INSERT INTO shop (id, image, type, stone, technology, cost, filter_5, description) VALUES (?,?,?,?,?,?,?,?)";
-  $stmt = mysqli_prepare($database, $query);
-  mysqli_stmt_bind_param($stmt, "isssssss", $new_id, $image, $type, $stone, $technology, $cost, $filter_5, $description);
-  if(mysqli_stmt_execute($stmt)){
-    mysqli_stmt_close($stmt);
-    return "Изображение успешно добавлено!";
-  }else{
-    return "Error in: " . $query . "<br>" . mysqli_error($database);
-  }
-}
-
-function add_in_archive($database, $image, $type, $stone, $technology, $cost, $filter_5, $description){
-  $query = "SELECT MAX(id) FROM archive";
-  if($result = mysqli_query($database, $query)){
-    while ($row = mysqli_fetch_row($result)) {
-      $new_id = array_pop($row) + 1;
-    }
-  }
-  $query = "INSERT INTO archive (id, image, type, stone, technology, cost, filter_5, description) VALUES (?,?,?,?,?,?,?,?)";
+  $query = "INSERT INTO photosession (id, image, type, stone, technology, cost, filter_5, description) VALUES (?,?,?,?,?,?,?,?)";
   $stmt = mysqli_prepare($database, $query);
   mysqli_stmt_bind_param($stmt, "isssssss", $new_id, $image, $type, $stone, $technology, $cost, $filter_5, $description);
   if(mysqli_stmt_execute($stmt)){
@@ -57,20 +39,9 @@ function change($database, $id, $derectory, $image, $type, $stone, $technology, 
   }
 }
 
-function get_shop($database){
+function get_photosession($database){
   $items = " ";
-  $query ="SELECT id, image, type, stone, technology, cost, filter_5, description  FROM shop";
-  if($result = mysqli_query($database, $query)){
-    while ($row = mysqli_fetch_row($result)) {
-      $items .= var_dump($row);
-    }
-  }
-  return $items;
-}
-
-function get_archive($database){
-  $items = " ";
-  $query ="SELECT id, image, type, stone, technology, cost, filter_5, description  FROM archive";
+  $query ="SELECT id, image, description, sub_img_1, sub_img_2, sub_img_3, sub_img_4, sub_img_5, sub_img_6, sub_img_7, sub_img_8, sub_img_9, sub_img_10, sub_img_11, sub_img_12, sub_img_13, sub_img_14, sub_img_15, sub_img_16, sub_img_17, sub_img_18, sub_img_19, sub_img_20, sub_img_21, sub_img_22, sub_img_23, sub_img_24, sub_img_25 FROM photosession";
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       $items .= var_dump($row);
