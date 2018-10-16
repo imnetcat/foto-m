ddbef66985405ba10da265dcbb32e5b74cead238
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-16 22:57:02 +0300

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -1,13 +1,13 @@
 <?
 
-function add_in_photosession($database, $image, $description, $sub_img_1, $sub_img_2, $sub_img_3, $sub_img_4, $sub_img_5, $sub_img_6, $sub_img_7, $sub_img_8, $sub_img_9, $sub_img_10, $sub_img_11, $sub_img_12, $sub_img_13, $sub_img_14, $sub_img_15, $sub_img_16, $sub_img_17, $sub_img_18, $sub_img_19, $sub_img_20, $sub_img_21, $sub_img_22, $sub_img_23, $sub_img_24, $sub_img_25){
+function add_in($database, $image, $derectory, $description, $sub_img_1, $sub_img_2, $sub_img_3, $sub_img_4, $sub_img_5, $sub_img_6, $sub_img_7, $sub_img_8, $sub_img_9, $sub_img_10, $sub_img_11, $sub_img_12, $sub_img_13, $sub_img_14, $sub_img_15, $sub_img_16, $sub_img_17, $sub_img_18, $sub_img_19, $sub_img_20, $sub_img_21, $sub_img_22, $sub_img_23, $sub_img_24, $sub_img_25){
   $query = "SELECT MAX(id) FROM photosession";
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       $new_id = array_pop($row) + 1;
     }
   }
-  $query = "INSERT INTO photosession ( id, image, description, sub_img_1, sub_img_2, sub_img_3, sub_img_4, sub_img_5, sub_img_6, sub_img_7, sub_img_8, sub_img_9, sub_img_10, sub_img_11, sub_img_12, sub_img_13, sub_img_14, sub_img_15, sub_img_16, sub_img_17, sub_img_18, sub_img_19, sub_img_20, sub_img_21, sub_img_22, sub_img_23, sub_img_24, sub_img_25) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+  $query = "INSERT INTO ".$derectory." ( id, image, description, sub_img_1, sub_img_2, sub_img_3, sub_img_4, sub_img_5, sub_img_6, sub_img_7, sub_img_8, sub_img_9, sub_img_10, sub_img_11, sub_img_12, sub_img_13, sub_img_14, sub_img_15, sub_img_16, sub_img_17, sub_img_18, sub_img_19, sub_img_20, sub_img_21, sub_img_22, sub_img_23, sub_img_24, sub_img_25) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
   $stmt = mysqli_prepare($database, $query);
   mysqli_stmt_bind_param($stmt, "issssssssssssssssssssssss", $new_id, $image, $description, $sub_img_1, $sub_img_2, $sub_img_3, $sub_img_4, $sub_img_5, $sub_img_6, $sub_img_7, $sub_img_8, $sub_img_9, $sub_img_10, $sub_img_11, $sub_img_12, $sub_img_13, $sub_img_14, $sub_img_15, $sub_img_16, $sub_img_17, $sub_img_18, $sub_img_19, $sub_img_20, $sub_img_21, $sub_img_22, $sub_img_23, $sub_img_24, $sub_img_25);
   if(mysqli_stmt_execute($stmt)){
@@ -39,9 +39,9 @@ function change($database, $id, $derectory, $image, $type, $stone, $technology, 
   }
 }
 
-function get_photosession($database){
+function get($database, $derectory){
   $items = " ";
-  $query ="SELECT id, image, description, sub_img_1, sub_img_2, sub_img_3, sub_img_4, sub_img_5, sub_img_6, sub_img_7, sub_img_8, sub_img_9, sub_img_10, sub_img_11, sub_img_12, sub_img_13, sub_img_14, sub_img_15, sub_img_16, sub_img_17, sub_img_18, sub_img_19, sub_img_20, sub_img_21, sub_img_22, sub_img_23, sub_img_24, sub_img_25 FROM photosession";
+  $query ="SELECT id, image, description, sub_img_1, sub_img_2, sub_img_3, sub_img_4, sub_img_5, sub_img_6, sub_img_7, sub_img_8, sub_img_9, sub_img_10, sub_img_11, sub_img_12, sub_img_13, sub_img_14, sub_img_15, sub_img_16, sub_img_17, sub_img_18, sub_img_19, sub_img_20, sub_img_21, sub_img_22, sub_img_23, sub_img_24, sub_img_25 FROM ".$derectory;
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       $items .= var_dump($row);
