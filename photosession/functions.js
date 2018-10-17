567c2dbc5a30c6652c8e5b2d8d54da9a1b8d3682
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-17 22:22:56 +0300

    Update functions.js

--- photosession/functions.js
+++ photosession/functions.js
@@ -96,7 +96,8 @@ function startLoading(allItems){
 }
 function continueLoading(th){
   var imgName = "";
-  var c = $(th).src.split("/")[6].split(".")[0].split("");
+  console.log($(th));
+  var c = $(this).src.split("/")[6].split(".")[0].split("");
   for(n = 0; n <= c.length; n++){
     if(c[n] == "-"){
       imgName += c[n];
