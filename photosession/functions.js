949d6928e681647f740f0cb5f32345ae1aa1585b
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-10-17 21:55:03 +0300

    Update functions.js

--- photosession/functions.js
+++ photosession/functions.js
@@ -96,15 +96,15 @@ function continueLoading(th){
       if(c[n+2]){
         if(c[n+3]){
           if(c[n+4]){
-            imgName += c[n+1]+c[n+2]+c[n+3]+c[n+4]+1;
+            imgName += Number(c[n+1]+c[n+2]+c[n+3]+c[n+4])+1;
           }else{
-            imgName += c[n+1]+c[n+2]+c[n+3]+1;
+            imgName += Number(c[n+1]+c[n+2]+c[n+3])+1;
           }
         }else{
-          imgName += c[n+1]+c[n+2]+1;
+          imgName += Number(c[n+1]+c[n+2])+1;
         }
       }else{
-        imgName += c[n+1]+1;
+        imgName += Number(c[n+1])+1;
       }
       n = c.length;
     }else{
